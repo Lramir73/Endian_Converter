c66733f8c4c1c14352469c0ab5ca7cb02808f261
Luis Ramirez <lramirz4@illinois.edu>
2015-11-25 23:13:31 -0600

    WIP

---
+++ Endian_Converter.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+#include <fstream>
+#define fileName "input"
+using namespace std;
+
+int main()
+{
+	ofstream file;
+	file.open(fileName);
+	
+
+
+	file.close();
+	return 0;
+}

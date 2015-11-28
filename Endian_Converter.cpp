0336a00239269799c285fcea63ed76c389aab96d
Lramir73 <Lramirz4@illinois.edu>
2015-11-27 21:12:02 -0600

    clean up

--- Endian_Converter.cpp
+++ Endian_Converter.cpp
@@ -1,16 +1,37 @@
 #include <iostream>
-#include <cstring>
-#include <fstream>
-#define fileName "input"
+#include <cstdio>
+#include <cstdlib>
+#include <cstdint>
 using namespace std;
 
 int main()
 {
-	ofstream file;
-	file.open(fileName);
-	
+	FILE *read = fopen("input.in", "rb");
+	FILE *write = fopen("output.out", "wb");
+
+	fseek(read, 0L, SEEK_END);
+	size_t size = ftell(read);
+	fseek(read, 0L, SEEK_SET);
+
+	uint8_t *dataRead = (uint8_t *) malloc(size);
+	uint8_t *dataWrite = (uint8_t *) malloc(size);
+
+    fread(dataRead, 1, size, read);
+
+	for (size_t i=0;i<size;i++)
+	{
+	    dataWrite[i] = (dataRead[i]>>4);
+	    printf("%X\n",dataWrite[i]);
+	}
 
 
-	file.close();
+
+    fwrite(dataWrite, 1, size, write);
+
+	free(dataRead);
+	free(dataWrite);
+
+	fclose(read);
+	fclose(write);
 	return 0;
 }
